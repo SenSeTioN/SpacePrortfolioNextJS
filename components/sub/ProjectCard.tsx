33e25270df5b27b5dd63b4eff9eb865633ddd026
SenSeTioN <lankovedik@gmail.com>
2024-01-02 19:39:30 +0300

    Initial commit

---
+++ components/sub/ProjectCard.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import { FC } from 'react'
+
+interface IProps {
+  src: string
+  title: string
+  description: string
+}
+
+const ProjectCard: FC<IProps> = ({ src, title, description }) => {
+  return (
+    <div className='relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]'>
+      <Image src={src} alt={title} width={1000} height={1000} className='w-full object-contain' />
+
+      <div className='relative p-4'>
+        <h1 className='text-2xl font-semibold text-white'>{title}</h1>
+        <p className='mt-2 text-gray-300'>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCard
---
+++ constants/index.types.ts
@@ -0,0 +1,8 @@
+export interface ISkillData {
+  skill_name: string
+  Image: string
+  width: number
+  height: number
+}
+
+export type TSocialData = Record<string, string>
